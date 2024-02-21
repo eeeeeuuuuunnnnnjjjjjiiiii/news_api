350b0f263fbe7ac8e6ab173803ab84646edb2bea
Lee eunji <144212524+eeeeeuuuuunnnnnjjjjjiiiii@users.noreply.github.com>
2024-02-22 01:19:40 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 const API_KEY=`a7b6acacf5f744089843abcad888acb8`;
-let news=[];
+let newsList=[];
 const getLatestNews = async () => {
     const url = new URL(
-        `https://eunji-news-api.netlify.app/top-headlines?q=아이유`
+        `https://eunji-news-api.netlify.app/top-headlines?country=kr%apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("eunji",response);
+    newsList = data.articles;
+    render();
+    console.log("eunji",newsList);
 };
 
+const render = () => {
+    const newsHTML = newsList.map(
+    (news) => ` <div class="row news">
+    <div class="col-lg-4">
+        <img class="news-img-size" 
+            src=${news.urlToImage}/>
+    </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>${news.description}</p>
+        <div>${news.source.name} * ${news.publishedAt}</div>
+</div>`
+);
+    
+    document.getElementById("news-board").innerHTML = newsHTML; 
+};
 
 getLatestNews();
